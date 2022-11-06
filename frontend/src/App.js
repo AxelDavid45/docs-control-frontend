0ac882da5379640b512a2cfed4d2836bb0dbe0c8
Axel Espinosa <aespinosa@fintonic.com>
2022-11-06 14:49:26 -0600

    feat: frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route, Navigate,
+} from 'react-router-dom'
+import { LayoutHome } from './containers/LayoutHome'
+import { Home } from './containers/Home'
+import { Routes } from './constants/routes'
+
+const loggedIn = true;
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path={Routes.Home} >
+          {loggedIn ?  <LayoutHome> <Home /> </LayoutHome>: <Navigate to={Routes.Login} />}
+        </Route>
+        <Route path={Routes.Login} >
+          {loggedIn ? <LayoutHome> <Home /> </LayoutHome> : <Navigate to={Routes.Login} />}
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
